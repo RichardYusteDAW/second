4a2c643f0660d57c4200156cd39538cca3753cc9
Richard Yuste <daw@richardyuste.com>
2024-09-17 20:54:55 +0200

    Update table exercise in Client

--- client/1ªEvaluacion/practices/tablas/tablas.js
+++ client/1ªEvaluacion/practices/tablas/tablas.js
@@ -1,17 +1,32 @@
 let num = null;
 do {
-    num = prompt('Escribe tu nombre: ');
-} while (!num);
+    num = prompt('Escribe un número: ');
+} while (!num)
 
 document.addEventListener('DOMContentLoaded', function () {
     const divTablas = document.getElementById('tablas');
 
-    let tablas = '<table border="1"><tr>'
+    let tablas = `<table border=1>
+                    <tr>
+                      <td></td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                    <td></td>
+                    <td>
+                      <table border=1><tr>`
     for (let i = 1; i <= 10; i++) {
-        tablas += `<td>${num} x ${i} = ${num * i}</td>`;
-        if ((i < 10) && (i % 2 === 0)) tablas += '</tr><tr>';
-        if (i === 10) tablas += '</tr>';
+        tablas += `<td>${num} x ${i} = ${num * i}</td>`
+        if ((i < 10) && (i % 2 == 0)) tablas += '</tr><tr>'
+        if (i === 10) tablas += '</tr>'
     }
-    tablas += '</table>';
+    // for (let i = 1; i <= 10; i++) {
+    //     tablas += `<td>${num}</td><td>x</td><td>${i}</td><td>=</td><td>${num * i}</td>`
+    //     if ((i < 10) && (i % 2 == 0)) tablas += '</tr><tr>'
+    //     if (i === 10) tablas += '</tr>'
+    // }
+    tablas += '</td></tr></table>';
+
     divTablas.innerHTML = tablas;
+
 });
