a7766c344324dcaa38b8048a215b176339556ea7
Richard Yuste <daw@richardyuste.com>
2024-10-15 12:58:27 +0200

    Added tienda in client

---
+++ client/1ªEvaluacion/practices/tienda/js/articulo.js
@@ -0,0 +1,17 @@
+class Articulo {
+    id;
+    name;
+    description;
+    price;
+    units;
+    total;
+
+    constructor(id, name, description, price, units) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.units = units;
+        this.total = price * units;
+    }
+}
---
+++ client/1ªEvaluacion/practices/tienda/js/tienda.js
@@ -0,0 +1,29 @@
+criterios = ["Sin ordenar", "Ascendente por precio", "Descendente por precio"]
+
+const creaListaCriterios = () => {
+
+}
+
+
+const pintaArticulos = (orden) => {
+
+}
+
+
+const ponArticuloEnCarrito = () => {
+
+}
+
+
+const verCarro = () => {
+
+}
+
+const efectuaPedido = () => {
+
+}
+
+window.onload = () => {
+
+}
+
