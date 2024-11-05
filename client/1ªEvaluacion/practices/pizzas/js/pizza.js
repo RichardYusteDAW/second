cb16ec15f5c116a37ad9973a0ab4bed75506aefb
Richard <daw@richardyuste.com>
2024-11-05 20:37:00 +0100

    Finished pizza exercise

--- client/1ªEvaluacion/practices/pizzas/js/pizza.js
+++ client/1ªEvaluacion/practices/pizzas/js/pizza.js
@@ -16,20 +16,56 @@ window.onload = () => {
 
     // Methods
     printSelect();
-
-    // Listeners
-    select.addEventListener("change", () => {
-        if (select.value != 'P0') {
-            fotopizza.innerHTML = `<img src="../img/${select.value}.jpg" alt="pizza">`;
-        }
-    });
 }
 
 
 /********** METHODS **********/
-const printSelect = () => {
+const printSelect1 = () => {
     let options = "";
     pizzas.forEach(p => {
         options += `<option value="${p.codigo}">${p.nombre}</option>`;
     });
+    select.innerHTML = options;
+}
+
+const printSelect = () => {
+    pizzas.forEach((p) => {
+        let option = document.createElement("option");
+        option.value = p.codigo;
+        option.text = p.nombre;
+        select.appendChild(option);
+    });
+    select.addEventListener("change", () => {
+        if (select.value === 'P0') {
+            fotopizza.innerHTML = "";
+        } else {
+            fotopizza.innerHTML = `<img src="../img/${select.value}.jpg" alt="pizza">`;
+        }
+        printImport(select.value);
+        printIngredients(select.value);
+    });
+}
+
+const printImport = (codigo) => {
+    let total = 0;
+    if (select.value !== 'P0') {
+        const pizza = pizzas.find(pizza => pizza.codigo == codigo);
+        total = pizza.ingredientes.reduce((acc, ingrediente) => acc + Number(ingrediente.precio), 5);
+    }
+
+    importe.innerHTML = `Importe: ${total}`;
+}
+
+const printIngredients = (codigo) => {
+    ingredientes.innerHTML = "";
+
+    if (select.value !== 'P0') {
+        const pizza = pizzas.find(pizza => pizza.codigo == codigo);
+        let content = "<ul>";
+        pizza.ingredientes.forEach((i) => {
+            content += `<li>${i.ingrediente}</li>`;
+        })
+        content += "</ul>";
+        ingredientes.innerHTML = content;
+    }
 }
