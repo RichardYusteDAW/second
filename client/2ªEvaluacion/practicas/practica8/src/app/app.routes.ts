26cdaf1b91d3b27e9b80875d02b4b55eeb2f8f14
Richard Yuste <daw@richardyuste.com>
2025-02-05 18:48:27 +0100

    Updated practise8 in client

--- client/2ªEvaluacion/practicas/practica8/src/app/app.routes.ts
+++ client/2ªEvaluacion/practicas/practica8/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { ArticlesComponent } from './components/articles/articles.component';
+import { ArticleComponent } from './components/article/article.component';
+import { NewArticleComponent } from './components/new-article/new-article.component';
 
 export const routes: Routes = [
     { path: '', component: ArticlesComponent },
     { path: 'articles', component: ArticlesComponent },
+    { path: 'articles/new', component: NewArticleComponent },
+    { path: 'article/:id', component: ArticleComponent }
 ];
--- client/2ªEvaluacion/practicas/practica8/src/app/components/articles/articles.component.ts
+++ client/2ªEvaluacion/practicas/practica8/src/app/components/articles/articles.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Article } from '../../models/article';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-articles',
@@ -13,7 +14,7 @@ export class ArticlesComponent {
   articles!: Array<Article>;
   url: string = 'http://localhost:3000/articles';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
     this.loadArticles();
@@ -24,5 +25,21 @@ export class ArticlesComponent {
       next: (data: any) => this.articles = data,
       error: error => console.log(error)
     });
+  }
+
+  newArticle() {
+    this.router.navigate(['/articles/new']);
+  }
+
+  showArticle(id: string) {
+    this.router.navigate(['/article', id]);
+  }
+
+  deleteArticle(id: string) {
+    this.http.delete(`${this.url}/${id}`).subscribe({
+      next: () => console.log('Article deleted'),
+      error: error => console.log(error)
+    });
+    this.loadArticles();
   }
 }
